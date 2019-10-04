e414f211b465a3df2d468d649f7f0ec7d454eb7a
Pat Crouch <crouchpat@gmail.com>
2019-10-03 22:16:45 -0700

    initial commit

---
+++ CalculatorAPI/Controllers/AddController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AddController : Controller
+    {
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get(int x, int y)
+        {
+            var total = x+y;
+            return total.ToString();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
